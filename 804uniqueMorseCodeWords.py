1c9b7ba771b3a5f5ddac2f58588bf5e32bf47cb4
christosMil <48249186+christosMil@users.noreply.github.com>
2021-02-26 23:51:45 +0200

    Update 804uniqueMorseCodeWords.py

--- 804uniqueMorseCodeWords.py
+++ 804uniqueMorseCodeWords.py
@@ -4,13 +4,12 @@ def uniqueMorseRepresentations(words: list) -> int:
 	morse_code = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
 	english_letters = list(ascii_lowercase)
 	letter_to_code = dict(zip(english_letters, morse_code))
-	res = [""]*len(words)
-	for i in range(len(words)):
-		word = list(words[i])
+	res = []
+	for i in words:
 		coded = ""
-		for j in word:
+		for j in i:
 			coded += letter_to_code[j]
-		res[i] = coded
+		res.extend([coded])
 	return len(set(res))
 
 words = ["gin", "zen", "gig", "msg"]
